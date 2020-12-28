a87a92a3d81fd0d4071af276869212c281c4e8c1
cpatterson71 <carl.pattersonsd@gmail.com>
2020-12-27 17:47:11 -0800

    added location layer to map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 //basemap for html
 var myMap = L.map("map", {
-center: [40.4759, -96.4001],
-zoom: 5,
+  center: [40.4759, -96.4001],
+  zoom: 5,
 });
 
 //added tilelayer for cities
@@ -18,42 +18,25 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // site for parsed data https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-d3.json(url).then(response=> {
-  var eventQuake = response.features
-  console.log("events", eventQuake)
+d3.json(url, function (response) {
+  var earthQuake = response.features
+  console.log("events", earthQuake)
 
-  eventQuake.forEach(d =>{
-    var depth = d.geometry.coordinates[2]
+//able to get data need to add the layers 
+  var earthLoc = []
 
-    var earthColor;
-    if (d.properties.mag > 7){
-      earthColor = "#5F0F40"}
-      else if (d.prperties.mag > 6){
-        earthColor = "#9A031E"}
-      else if (d.properties.mag > 5){
-        earthColor = "#A94A0F"}
-      else if (d.properties.mag > 4){
-        earthColor = "#F88B24"}
-      else if (d.properties.mag > 3){
-        earthColor = "#0F4C5C"}
-      else {
-        earthColor = "#757575"}
-      });
+  earthQuake.forEach(d => {
+    var location = (d.geometry.coordinates[0], d.geometry.coordinates[1])
+    console.log(location)
 
-    var magMulti;
-    if (d.properties.mag >7){
-      magMulti = d.properties.mag * 70000}
-      else if (d.properties.mag >6){
-        magMulti = d.properties.mag * 60000}
-      else if (d.properties.mag >5){
-       magMulti = d.properties.mag * 50000}
-      else if (d.properties.mag >4){
-        magMulti = d.properties.mag * 40000}
-      else if (d.properties.mag >3){
-        magMulti = d.properties.mag * 30000}
-      else {
-        magMulti = d.properties.mag * 20000}
-      }
-    };
-  };
-});
+    
+  })
+
+//first layer is the coordinates
+//second layer is the magnitude
+//final is to add the legend
+
+  // earthquake.forEach(d => {
+  //   var depth = d.geometry.coordinates[2];
+  //   console.log(depth)
+  });
