082345ece831feb9feffa50a41792b45bf632d04
cpatterson71 <carl.pattersonsd@gmail.com>
2020-12-27 19:53:32 -0800

    adding coordinates

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,25 +18,31 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // site for parsed data https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-d3.json(url, function (response) {
-  var earthQuake = response.features
-  console.log("events", earthQuake)
-
-//able to get data need to add the layers 
-  var earthLoc = []
-
-  earthQuake.forEach(d => {
-    var location = (d.geometry.coordinates[0], d.geometry.coordinates[1])
-    console.log(location)
-
-    
-  })
-
+//able to get data need to add the layers
+d3.json(url, function(response){
+  //console.log(response);
+  
 //first layer is the coordinates
+  for (var i=0; i <response.length; i++){
+    var location = response[i].location;
+
+    if (location) {
+      L.marker([location.geometry.coordinates[1], location.geometry.coordinates[0]]).addTo(myMap);
+    }
+  console.log(location);
+}
+});
+
+  
+
+ 
+
+
 //second layer is the magnitude
+
+
 //final is to add the legend
 
   // earthquake.forEach(d => {
   //   var depth = d.geometry.coordinates[2];
   //   console.log(depth)
-  });
